854511395d33d5729abc06a8499411f25bd6f8b7
Darko Kukovec <darko@infinum.hr>
2020-08-12 16:34:31 +0200

    Bugfix

--- src/withBugsnagNextApp.tsx
+++ src/withBugsnagNextApp.tsx
@@ -1,5 +1,5 @@
 import { Bugsnag } from '@bugsnag/js';
-import { NextAppContext } from 'next/app';
+import NextAppContext from 'next/app';
 import React, { Component } from 'react';
 
 import { BugsnagProvider } from './context';
@@ -8,10 +8,12 @@ import { wrapDisplayName } from './utils/displayName';
 import { getOrCreateBugsnagClient } from './utils/getOrCreateBugsnagClient';
 
 export const withBugsnagNextApp = (apiKeyOrOpts: string | Bugsnag.IConfig) => (
-  ComposedComponent: any,
+  ComposedComponent: any
 ): any => {
   class WithBugsnagNextAppWrapper extends Component {
-    public static async getInitialProps(context: NextAppContext & IBugsnagNextAppContext): Promise<any> {
+    public static async getInitialProps(
+      context: NextAppContext & IBugsnagNextAppContext
+    ): Promise<any> {
       let appProps = {
         pageProps: {},
       };
@@ -48,7 +50,10 @@ export const withBugsnagNextApp = (apiKeyOrOpts: string | Bugsnag.IConfig) => (
   }
 
   if (process.env.NODE_ENV !== 'production') {
-    (WithBugsnagNextAppWrapper as any).displayName = wrapDisplayName(ComposedComponent, 'withBugsnagNextApp');
+    (WithBugsnagNextAppWrapper as any).displayName = wrapDisplayName(
+      ComposedComponent,
+      'withBugsnagNextApp'
+    );
   }
 
   return WithBugsnagNextAppWrapper;
